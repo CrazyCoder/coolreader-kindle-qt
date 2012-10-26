d8daec3c4a2dff5377783a47f851482653733ab4
Serge Baranov <sbaranov@gmail.com>
2012-10-26 09:22:44 +0400

    better suspend/resume scripts

--- dist-kt/cr3/ktresume.sh
+++ dist-kt/cr3/ktresume.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/mnt/us/cr3/lib
 
-for i in $(xwininfo -tree -root | grep "com.lab126.booklet.kindlet_M" | awk '{ print $1 }' ); do 
-  ./xdotool windowmap $i
-done 
+if [ -s ".unmapped" ]
+then
+  cat .unmapped | while read id ; do echo $id >> .unmapped; ./xdotool windowmap $id ; done
+  rm -f .unmapped
+else
+  for name in blankBackground_WS searchBar_ID com.lab126.booklet.home_M titleBar_ID keyboard; do
+    ./xdotool windowmap `xwininfo -tree -root | grep "$name" | awk '{ print $1 }'`
+  done
+fi
 
 lipc-set-prop com.lab126.appmgrd start app://com.lab126.booklet.home
-./xdotool windowactivate `xwininfo -tree -root | grep "blankBackground_WS" | awk '{ print $1 }'`
-./xdotool windowactivate `xwininfo -tree -root | grep "searchBar_ID" | awk '{ print $1 }'`
-./xdotool windowmap      `xwininfo -tree -root | grep "com.lab126.booklet.home_M" | awk '{ print $1 }'`
-./xdotool windowmap      `xwininfo -tree -root | grep "titleBar_ID" | awk '{ print $1 }'`
--- dist-kt/cr3/ktsuspend.sh
+++ dist-kt/cr3/ktsuspend.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 export LD_LIBRARY_PATH=/mnt/us/cr3/lib
 
-./xdotool windowminimize   `xwininfo -tree -root | grep "searchBar_ID" | awk '{ print $1 }'`
-./xdotool windowminimize   `xwininfo -tree -root | grep "blankBackground_WS" | awk '{ print $1 }'`
-./xdotool windowunmap      `xwininfo -tree -root | grep "com.lab126.booklet.home_M" | awk '{ print $1 }'`
-./xdotool windowunmap      `xwininfo -tree -root | grep "titleBar_ID" | awk '{ print $1 }'`
-
-for i in $(xwininfo -tree -root | grep "com.lab126.booklet.kindlet_M" | awk '{ print $1 }' ); do 
-  ./xdotool windowunmap $i
-done
-
-COUNTER=4; until [ $COUNTER -lt 1 ]; do ./xdotool search --classname --onlyvisible Pillowd windowminimize;let COUNTER-=1; done
-COUNTER=4; until [ $COUNTER -lt 1 ]; do ./xdotool search --classname --onlyvisible Webreader windowminimize;let COUNTER-=1; done
+# unmap all viewable windows
+rm -f .unmapped
+xwininfo -tree -root | grep "MapState=IsViewable" | awk '{ print $1 }' | while read id ; do echo $id >> .unmapped; ./xdotool windowunmap $id ; done
