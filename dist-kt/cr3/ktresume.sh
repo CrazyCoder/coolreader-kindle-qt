73230631ee14532b18d79507f71ef1e87c42c33f
Serge Baranov <sbaranov@gmail.com>
2013-11-22 14:13:42 +0400

    better and faster suspend/resume, the trick is to make the blank background window fullscreen instead of hiding/restoring all the windows

--- dist-kt/cr3/ktresume.sh
+++ dist-kt/cr3/ktresume.sh
@@ -2,5 +2,5 @@
 killall -CONT awesome
 # full resume
 if [ "$1" -eq "0" ]; then
-  /var/tmp/wmctrl -l | awk '{ print $1 }' | while read id ; do /var/tmp/wmctrl -i -r $id -b remove,hidden ; done
+  /var/tmp/wmctrl -r blankBackground_WS -b remove,fullscreen
 fi
--- dist-kt/cr3/ktsuspend.sh
+++ dist-kt/cr3/ktsuspend.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 # full suspend
-#if [ "$1" -eq "0" ]; then
-/var/tmp/wmctrl -l | awk '{ print $1 }' | while read id ; do /var/tmp/wmctrl -i -r $id -b add,hidden ; done
-#fi
-
+/var/tmp/wmctrl -r blankBackground_WS -b add,fullscreen
 killall -STOP awesome
