5173a29563158b70098753d2e61bd7be5f771302
Serge Baranov <sbaranov@gmail.com>
2013-12-04 19:23:06 +0400

    faster start if crfswin fails

--- dist-kt/cr3/ktsuspend.sh
+++ dist-kt/cr3/ktsuspend.sh
@@ -3,7 +3,7 @@ rm -rf /var/tmp/crfswin.running
 /var/tmp/crfswin &
 # wait for the flag file
 x=0
-while [ "$x" -lt 50 -a ! -f /var/tmp/crfswin.running ]
+while [ "$x" -lt 30 -a ! -f /var/tmp/crfswin.running ]
 do
   x=$((x+1))
   /var/tmp/usleep 200000
